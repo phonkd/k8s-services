3463f146ba6f7df74f56c9df3725dd9f1c12f121
Elis <enst18.12@gmail.com>
2025-02-26 22:26:17 +0100

    non working non sigma

--- docker-services/202-devni/configuration.nix
+++ docker-services/202-devni/configuration.nix
@@ -100,5 +100,5 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
   programs.zsh.enable = true;
-
+  services.qemuGuest.enable = true;
 }
