d8ed59f465ad1341781df578e1991bcf39608bce
Elis <enst18.12@gmail.com>
2025-03-19 00:38:45 +0100

    not working temp

--- nix-k8s/9201-nix-m01/configuration.nix
+++ nix-k8s/9201-nix-m01/configuration.nix
@@ -2,14 +2,18 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, lib,... }:
 
 {
+  k8s.kubeMasterIP = "192.168.90.201";
+  #k8s.kubeMasterHostname = "mykube.local";
+  #k8s.kubeMasterAPIServerPort = 6443;
+  
   imports =
     [ # Include the results of the hardware scan.
       ../base/base.nix
+      ../base/k8s-master.nix
       ../base/base-hardware-configuration.nix
-      ./k8s.nix
       ./network.nix
     ];
 }
