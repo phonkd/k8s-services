2cfeadf240635a0294976c293d93a0b51c52c3ff
Elis <enst18.12@gmail.com>
2025-03-19 11:50:43 +0100

    add pubkeyauth fixed

--- nix/base/base.nix
+++ nix/base/base.nix
@@ -29,7 +29,6 @@
   };
   nixpkgs.config.allowUnfree = true;
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
   networking.firewall.enable = true;
   system.stateVersion = "24.11"; # Did you read the comment?
   programs.zsh.enable = true;
@@ -42,10 +41,10 @@
     settings.PermitRootLogin = "yes";
   };
   users.users."root".openssh.authorizedKeys.keys = [
-    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDg0PjpVeFevKuUq7ZVAhL0fySgOomRT/SZ6jWFxfv0q06KgwLSInwXFZDIUNN9c2Uz6qgJvh/xZ9UQfuoYwBMwUDt89hhplZDeFG+0kTxPRyjKrtcOXefM2ne4eI93kvJfU5+SaxXs3GF5oChoml4Wwub74CVLWIlKTvA7YLEKzBffEJ4ypO97YTR734Cd1vHsIOVFylftIpe0n/oA7o3Bu+GSRwfW4cM9nbYcumydwyrA9osrQ6dLNFCJ6DSvBY65j9eU/wGEObmch645f+hAm1ROZxoUYtVBQjSNheYNIUAxjXDbHd/eA3TjG6qGfUSbFu1gitQBLY4M+YUmT+r/IjD3XBFwFCED3G/TKKBjKubCMk0yxegCa+JZt+HzSbRTILgFv0eC+DvZBgMHMx0RjefvOJY6mCWtwwYRULp+2ulls6RTX2F3aEEKO0+/9YxTfzvwE1zFLAVxNpCg25f35eWuBdIJD/2K42Krbe2xrGDJdFhRtpT1uoq0qGHreIk= phonkd@Eliss-MacBook-Pro.local
-    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHhekaG8W8NVR7/OJCUGaBQSwWgBFmBpdcxaqx7V6qDp phonkd@dev-vm
-    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJxyCQHxLqEsSRPGG5SeN4MsOGoc9mrrEioU2mQ9Xz4z phonkd@nixos
-    " # content of authorized_keys file
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDg0PjpVeFevKuUq7ZVAhL0fySgOomRT/SZ6jWFxfv0q06KgwLSInwXFZDIUNN9c2Uz6qgJvh/xZ9UQfuoYwBMwUDt89hhplZDeFG+0kTxPRyjKrtcOXefM2ne4eI93kvJfU5+SaxXs3GF5oChoml4Wwub74CVLWIlKTvA7YLEKzBffEJ4ypO97YTR734Cd1vHsIOVFylftIpe0n/oA7o3Bu+GSRwfW4cM9nbYcumydwyrA9osrQ6dLNFCJ6DSvBY65j9eU/wGEObmch645f+hAm1ROZxoUYtVBQjSNheYNIUAxjXDbHd/eA3TjG6qGfUSbFu1gitQBLY4M+YUmT+r/IjD3XBFwFCED3G/TKKBjKubCMk0yxegCa+JZt+HzSbRTILgFv0eC+DvZBgMHMx0RjefvOJY6mCWtwwYRULp+2ulls6RTX2F3aEEKO0+/9YxTfzvwE1zFLAVxNpCg25f35eWuBdIJD/2K42Krbe2xrGDJdFhRtpT1uoq0qGHreIk= phonkd@Eliss-MacBook-Pro.local"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHhekaG8W8NVR7/OJCUGaBQSwWgBFmBpdcxaqx7V6qDp phonkd@dev-vm"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJxyCQHxLqEsSRPGG5SeN4MsOGoc9mrrEioU2mQ9Xz4z phonkd@nixos"
+    # content of authorized_keys file
     # note: ssh-copy-id will add user@your-machine after the public key
     # but we can remove the "@your-machine" part
   ];
