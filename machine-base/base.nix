293eff29e56ee9d511c37765f6a9e7c419e0e996
Phonkd <enst18.12@gmail.com>
2025-04-04 23:49:21 +0200

    change to user

--- machine-base/base.nix
+++ machine-base/base.nix
@@ -38,9 +38,9 @@
     # require public key authentication for better security
     settings.PasswordAuthentication = false;
     settings.KbdInteractiveAuthentication = false;
-    settings.PermitRootLogin = "yes";
+    settings.PermitRootLogin = "no";
   };
-  users.users."root".openssh.authorizedKeys.keys = [
+  users.users."phonkd".openssh.authorizedKeys.keys = [
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDg0PjpVeFevKuUq7ZVAhL0fySgOomRT/SZ6jWFxfv0q06KgwLSInwXFZDIUNN9c2Uz6qgJvh/xZ9UQfuoYwBMwUDt89hhplZDeFG+0kTxPRyjKrtcOXefM2ne4eI93kvJfU5+SaxXs3GF5oChoml4Wwub74CVLWIlKTvA7YLEKzBffEJ4ypO97YTR734Cd1vHsIOVFylftIpe0n/oA7o3Bu+GSRwfW4cM9nbYcumydwyrA9osrQ6dLNFCJ6DSvBY65j9eU/wGEObmch645f+hAm1ROZxoUYtVBQjSNheYNIUAxjXDbHd/eA3TjG6qGfUSbFu1gitQBLY4M+YUmT+r/IjD3XBFwFCED3G/TKKBjKubCMk0yxegCa+JZt+HzSbRTILgFv0eC+DvZBgMHMx0RjefvOJY6mCWtwwYRULp+2ulls6RTX2F3aEEKO0+/9YxTfzvwE1zFLAVxNpCg25f35eWuBdIJD/2K42Krbe2xrGDJdFhRtpT1uoq0qGHreIk= phonkd@Eliss-MacBook-Pro.local"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHhekaG8W8NVR7/OJCUGaBQSwWgBFmBpdcxaqx7V6qDp phonkd@dev-vm"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJxyCQHxLqEsSRPGG5SeN4MsOGoc9mrrEioU2mQ9Xz4z phonkd@nixos"
