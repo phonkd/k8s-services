ace4fd3ceccbe93b4f4ebdc3f1e044312e302071
Phonkd <enst18.12@gmail.com>
2025-04-05 15:53:51 +0200

    me retarded

--- machines-nok8s/121-nix-services/flake.nix
+++ machines-nok8s/121-nix-services/flake.nix
@@ -1,34 +1,31 @@
 {
-  description = "NixOS system with custom Caddy and powerdns plugin";
+  description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11"; # or your stable
-    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, unstable, flake-utils, ... }@inputs:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
+  outputs = { self, nixpkgs, nixpkgs-unstable }: {
 
-        unstablePkgs = import unstable {
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    let
+      system = "x86_64-linux";
+      overlay-unstable = final: prev: {
+        unstable = import nixpkgs-unstable {
           inherit system;
           config.allowUnfree = true;
         };
-      in {
-        nixosConfigurations.nixos-services = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            ./configuration.nix
-            # Pass unstable as a module argument
-            ({ config, pkgs, ... }: {
-              _module.args.unstable = unstablePkgs;
-            })
-          ];
-        };
-      });
+      };
+    in {
+      nixosConfigurations."nixos-services" = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          # Overlays-module makes "pkgs.unstable" available in configuration.nix
+          ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
+          ./configuration.nix
+        ];
+      };
+    };
 }
