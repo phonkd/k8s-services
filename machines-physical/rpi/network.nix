6379f3aceeaf31940d01054c32240f48295f3605
phonkd <enst18.12@gmail.com>
2025-04-05 19:36:43 +0200

    enable wifi and networkmanager

--- machines-physical/rpi/network.nix
+++ machines-physical/rpi/network.nix
@@ -16,4 +16,11 @@
       address = "192.168.1.123";
       prefixLength = 24;
   } ];
+  networking.interfaces.wlan0.ipv4.addresses = [ {
+      address = "192.168.1.122";
+      prefixLength = 24;
+  } ];
+  networking.networkmanager.enable = true;
+    # Prevent host becoming unreachable on wifi after some time.
+  networking.networkmanager.wifi.powersave = false;
 }
