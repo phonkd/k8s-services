2d95b0446188653f22ae794d1515e263e16ca648
Phonkd <enst18.12@gmail.com>
2025-04-05 21:54:32 +0200

    idk

--- machines-physical/rpi/rpi-services.nix
+++ machines-physical/rpi/rpi-services.nix
@@ -11,7 +11,7 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # Uncomment the following line if you want to use JACK applications        # jack.enable = true;
+    jack.enable = true;
   };
     #
     # Bluetooth
