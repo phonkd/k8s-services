081d2475f60afa12dab44d7bd3542e6eb09c1030
Phonkd <enst18.12@gmail.com>
2025-04-05 16:12:15 +0200

    wtf is that stupid tag

--- machines-nok8s/apps/reverseproxy.nix
+++ machines-nok8s/apps/reverseproxy.nix
@@ -2,7 +2,7 @@
 {
   services.caddy = {
     package = pkgs.unstable.caddy.withPlugins {
-      plugins = [ "github.com/caddy-dns/cloudflare@latest" ];
+      plugins = [ "github.com/caddy-dns/cloudflare@v0.0.0-20250228175314-1fb64108d4de" ];
       hash = "sha256-YYpsf8HMONR1teMiSymo2y+HrKoxuJMKIea5/NEykGc=";
     };
     enable = true;
