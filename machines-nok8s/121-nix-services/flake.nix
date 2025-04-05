c180c1f02783fa6f9f303e7540fa67fec0580ea7
Phonkd <enst18.12@gmail.com>
2025-04-05 16:01:09 +0200

    wtf4

--- machines-nok8s/121-nix-services/flake.nix
+++ machines-nok8s/121-nix-services/flake.nix
@@ -2,7 +2,8 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs, nixpkgs-unstable }:
