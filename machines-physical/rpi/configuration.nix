1aca4ddf1dee2011318a1491b1eceda91794138c
Phonkd <enst18.12@gmail.com>
2025-04-05 22:26:54 +0200

    test different audio configuations

--- machines-physical/rpi/configuration.nix
+++ machines-physical/rpi/configuration.nix
@@ -7,6 +7,8 @@
       ./rpi-hw.nix
       #/etc/nixos/hardware-configuration.nix
       ./raspotify-container.nix
+      "${builtins.fetchGit { url = "https://github.com/NixOS/nixos-hardware.git"; }}/raspberry-pi/4"
     ];
   system.stateVersion = "24.11";
+  hardware.raspberry-pi."4".audio.enable = true;
 }
