e47ed724c26a578cf66407de49ada048e9b45d1c
Phonkd <enst18.12@gmail.com>
2025-04-05 15:12:03 +0200

    how to break os cuz flake complicated

---
+++ machines-nok8s/121-nix-services/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "NixOS system with custom Caddy and powerdns plugin";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11"; # or your stable
+    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, unstable, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+
+        unstablePkgs = import unstable {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in {
+        nixosConfigurations.your-hostname = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./configuration.nix
+            # Pass unstable as a module argument
+            ({ config, pkgs, ... }: {
+              _module.args.unstable = unstablePkgs;
+            })
+          ];
+        };
+      });
+}
--- machines-nok8s/apps/reverseproxy.nix
+++ machines-nok8s/apps/reverseproxy.nix
@@ -1,13 +1,10 @@
-{ config, pkgs, ... }:
-let
-  unstable = import <nixos-unstable> {};
-in
+{ config, pkgs, unstable, ... }:
 {
   services.caddy = {
     package = unstable.caddy.withPlugins {
-        plugins = [ "github.com/caddy-dns/powerdns@v1.0.1" ];
-        hash = "sha256-F/jqR4iEsklJFycTjSaW8B/V3iTGqqGOzwYBUXxRKrc=";
-      };
+      plugins = [ "github.com/caddy-dns/powerdns@v1.0.1" ];
+      hash = "sha256-F/jqR4iEsklJFycTjSaW8B/V3iTGqqGOzwYBUXxRKrc=";
+    };
     enable = true;
     virtualHosts."vw.nix-services.phonkd.net".extraConfig = ''
       reverse_proxy :8222
