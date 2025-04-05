0608981e1282bf6908623459c69b4e9597dbdb29
phonkd <enst18.12@gmail.com>
2025-04-05 21:19:58 +0200

    asdf

--- machines-physical/rpi/network.nix
+++ machines-physical/rpi/network.nix
@@ -12,23 +12,11 @@
   # Groups:
   programs.ssh.startAgent = true; #ssh-agent
   networking.firewall.enable = false;
-  # networking.interfaces.end0.ipv4.addresses = [ {
-  #     address = "192.168.1.123";
+  # networking.interfaces.wlan0.ipv4.addresses = [ {
+  #     address = "192.168.1.122";
   #     prefixLength = 24;
   # } ];
-  networking.interfaces.wlan0.ipv4.addresses = [ {
-      address = "192.168.1.122";
-      prefixLength = 24;
-  } ];
-  networking.networkmanager.enable = lib.mkForce false;
+  networking.networkmanager.enable = lib.mkForce true;
     # Prevent host becoming unreachable on wifi after some time.
-  #networking.networkmanager.wifi.powersave = false;
-
-  networking = {
-    wireless = {
-        enable = true;
-        # Enable the wireless interface
-        interfaces = [ "wlan0" ];
-    };
-  };
+  networking.networkmanager.wifi.powersave = false;
 }
