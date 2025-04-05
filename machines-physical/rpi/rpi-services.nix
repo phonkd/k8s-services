84f88f51c442d5e0484d76297d2baacf4d833cd4
phonkd <enst18.12@gmail.com>
2025-04-05 18:52:19 +0200

    experiment with rpi4 2

--- machines-physical/rpi/rpi-services.nix
+++ machines-physical/rpi/rpi-services.nix
@@ -1,9 +1,9 @@
 { config, pkgs, ... }:
 {
-  boot.kernelParams = [ "snd_bcm2835.enable_hdmi=1" "snd_bcm2835.enable_headphones=1" ];
-  boot.loader.raspberryPi.firmwareConfig = ''
-    dtparam=audio=on
-  '';
+  # boot.kernelParams = [ "snd_bcm2835.enable_hdmi=1" "snd_bcm2835.enable_headphones=1" ];
+  # boot.loader.raspberryPi.firmwareConfig = ''
+  #   dtparam=audio=on
+  # '';
   hardware.pulseaudio.enable = false; # Use Pipewire, the modern sound subsystem
   security.rtkit.enable = true; # Enable RealtimeKit for audio purposes
   services.pipewire = {
