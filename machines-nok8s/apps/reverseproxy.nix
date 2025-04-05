80d7ef7190a58a0ac83d581a196439e0784c628f
Phonkd <enst18.12@gmail.com>
2025-04-05 16:10:07 +0200

    miiimiiimii hash mismatch iz machi nuet

--- machines-nok8s/apps/reverseproxy.nix
+++ machines-nok8s/apps/reverseproxy.nix
@@ -3,7 +3,7 @@
   services.caddy = {
     package = pkgs.unstable.caddy.withPlugins {
       plugins = [ "github.com/caddy-dns/cloudflare@latest" ];
-      #hash = "sha256-F/jqR4iEsklJFycTjSaW8B/V3iTGqqGOzwYBUXxRKrc=";
+      hash = "sha256-YYpsf8HMONR1teMiSymo2y+HrKoxuJMKIea5/NEykGc=";
     };
     enable = true;
     virtualHosts."vw.nix-services.phonkd.net".extraConfig = ''
