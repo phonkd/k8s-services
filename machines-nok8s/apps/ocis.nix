6a42552e004256d619d909af08d229f21ec742db
phonkd <enst18.12@gmail.com>
2025-04-06 03:04:48 +0000

    idk

--- machines-nok8s/apps/ocis.nix
+++ machines-nok8s/apps/ocis.nix
@@ -11,14 +11,15 @@
   ## above part is for loading config as secret
   services.ocis = {
     enable = true;
-    address = "127.0.0.1";
+    #address = "127.0.0.1";
     url = "https://localhost:9200";
     configDir = "/run/secrets";
     environment = {
-      #OCIS_INSECURE = "true";
-      #CS3_ALLOW_INSECURE = "true";
-      #TLS_SKIP_VERIFY_CLIENT_CERT = "true";
-      #OCIS_INSECURE_BACKENDS = "true";
+      OCIS_INSECURE = "true";
+      CS3_ALLOW_INSECURE = "true";
+      TLS_SKIP_VERIFY_CLIENT_CERT = "true";
+      OCIS_INSECURE_BACKENDS = "true";
+      IDP_ISS = "https://localhost:9200";
     };
   };
   services.caddy = {
@@ -31,7 +32,6 @@
           tls_insecure_skip_verify
         }
       }
-
     '';
   };
 }
