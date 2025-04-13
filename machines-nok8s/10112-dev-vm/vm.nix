b1058438ffc3c6f702b79eaf5d2b3beb7479d87c
Phonkd <enst18.12@gmail.com>
2025-04-13 14:42:55 +0200

    fix secret

--- machines-nok8s/10112-dev-vm/vm.nix
+++ machines-nok8s/10112-dev-vm/vm.nix
@@ -25,7 +25,7 @@
     teleport = {
       nodename = "nixvm";
       # advertise_ip = "192.168.90.187";
-      auth_token = builtins.readFile"config.sops.secrets."teleport-authkey".path}";
+      auth_token = "${builtins.readFile config.sops.secrets."cfapikey".path}";
       #auth_servers = [ "freakedyproxy.teleport.phonkd.net" ];
       proxy_server = "teleport.phonkd.net:443";
     };
