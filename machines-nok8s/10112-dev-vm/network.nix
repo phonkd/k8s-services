aeb38f3145a5f4467000e76d3eed210fb37e3540
Phonkd <enst18.12@gmail.com>
2025-04-13 14:28:00 +0200

    asdf

--- machines-nok8s/10112-dev-vm/network.nix
+++ machines-nok8s/10112-dev-vm/network.nix
@@ -10,7 +10,7 @@
       prefixLength = 24;
   } ];
   networking.defaultGateway = "192.168.1.1";
-  networking.nameservers = [ "192.168.90.1" ];
+  networking.nameservers = [ "192.168.1.3" ];
   networking.hostName = "dev-vm"; # Define your hostname.
   networking.networkmanager.dhcp = "internal";
   # Groups:
