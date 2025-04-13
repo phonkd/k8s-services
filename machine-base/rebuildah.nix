f5555b6307cd6cd75c439e04819f30138adf57b6
Phonkd <enst18.12@gmail.com>
2025-04-13 16:59:08 +0200

    probably break all vms

--- machine-base/rebuildah.nix
+++ machine-base/rebuildah.nix
@@ -3,7 +3,7 @@ pkgs.stdenv.mkDerivation {
   name = "nix-experiment";
   src = pkgs.fetchurl {
     url = "https://github.com/phonkd/nix-experiment/releases/download/0.1/nix-experiment-0.1-linux-amd64.tar.gz";
-    #sha256 = "0ldh303r5063kd5y73hhkbd9v11c98aki8wjizmchzx2blwlipy7";
+    sha256 = "sha256-Aht4ywR1fo5PQ1clBA4K9dXdGlYLgvkYpGRxu8tnV8w=";
   };
   phases = ["installPhase" "patchPhase"];
   installPhase = ''
