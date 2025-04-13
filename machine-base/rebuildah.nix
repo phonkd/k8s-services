3a03d60a66c5d4a37642f696a3f2e1348bc55ff7
Phonkd <enst18.12@gmail.com>
2025-04-13 16:41:02 +0200

    probably break all vms

--- machine-base/rebuildah.nix
+++ machine-base/rebuildah.nix
@@ -4,6 +4,6 @@
     [
       "${builtins.fetchTarball {
         url = "https://github.com/phonkd/nix-experiment/releases/download/0.1/nix-experiment-0.1-linux-amd64.tar.gz";
-      }}/modules/rebuild-wrapper"
+      }}/rebuild-wrapper
     ];
 }
