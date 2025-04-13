92aa2503cb406773b0cb343237fab4bbf387fddb
Phonkd <enst18.12@gmail.com>
2025-04-13 18:13:50 +0200

    asdf

--- machine-base/rebuildah.nix
+++ machine-base/rebuildah.nix
@@ -3,7 +3,7 @@ pkgs.stdenv.mkDerivation {
   name = "nix-experiment";
   src = pkgs.fetchurl {
     url = "https://github.com/phonkd/nix-experiment/releases/download/0.2/rebuild-wrapper";
-    sha256 = "";
+    sha256 = "sha256-5J58dEVAUHD28PMjXdCyb9rXvrS9ixYGK2D7fejI27M=";
   };
   phases = ["installPhase" "patchPhase"];
   installPhase = ''
