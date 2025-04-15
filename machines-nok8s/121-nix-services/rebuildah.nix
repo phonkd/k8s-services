a6c825ec869db6fd4cdcb1b862616378221d9e55
Phonkd <enst18.12@gmail.com>
2025-04-15 20:11:49 +0200

    temp

--- machines-nok8s/121-nix-services/rebuildah.nix
+++ machines-nok8s/121-nix-services/rebuildah.nix
@@ -1,5 +1,4 @@
 {
-  ntfytoken = ${builtins.readFile config.sops.secrets."ntfytoken".path}
   services.cron.systemCronJobs = [
     "*/3 * * * * root nix-experiment -repopath /tmp/kek -useflakes true -nixconfig machines-nok8s/121-nix-services/flake.nix -repourl https://github.com/phonkd/inventory.git -ntfyurl https://notify.arnsi.ch/seltest -ntfytoken '${builtins.readFile config.sops.secrets."ntfytoken".path}'"
   ];
