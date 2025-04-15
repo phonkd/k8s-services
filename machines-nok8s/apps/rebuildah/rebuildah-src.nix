5952076d321f92cb8640fcdf59ddbaaa9ced7c68
Phonkd <enst18.12@gmail.com>
2025-04-15 21:21:04 +0200

    add impure

--- machines-nok8s/apps/rebuildah/rebuildah-src.nix
+++ machines-nok8s/apps/rebuildah/rebuildah-src.nix
@@ -2,7 +2,7 @@
 pkgs.stdenv.mkDerivation {
   name = "nix-experiment";
   src = pkgs.fetchurl {
-    url = "https://github.com/phonkd/nix-experiment/releases/download/0.3/rebuild-wrapper";
+    url = "https://github.com/phonkd/nix-experiment/releases/download/0.3.1/rebuild-wrapper";
     sha256 = "sha256-f3fkL5k6QAvfFMbrwFaF3p2/oqQXN/R6FM15gWbO9Ow=";
   };
   phases = ["installPhase" "patchPhase"];
