26631962f327cc23602588ba8cc552707a0ee5a2
Phonkd <enst18.12@gmail.com>
2025-04-15 20:33:16 +0200

    tryfix

--- machines-nok8s/121-nix-services/rebuildah.nix
+++ machines-nok8s/121-nix-services/rebuildah.nix
@@ -2,6 +2,9 @@
   imports = [
     ../machines-nok8s/apps/rebuildah/rebuildah.nix
   ];
+  sops.secrets.ntfytoken = {
+    sopsFile = ../apps/rebuildah/secrets/secrets.yaml;
+  };
   services.cron.systemCronJobs = [
     "*/3 * * * * root nix-experiment -repopath /tmp/kek -useflakes true -nixconfig machines-nok8s/121-nix-services/flake.nix -repourl https://github.com/phonkd/inventory.git -ntfyurl https://notify.arnsi.ch/seltest -ntfytoken '${builtins.readFile config.sops.secrets."ntfytoken".path}'"
   ];
