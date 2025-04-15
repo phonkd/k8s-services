fef2c4a14da404ab59acf82126db903e3a825b51
Phonkd <enst18.12@gmail.com>
2025-04-15 19:46:15 +0200

    atry movin stuff around

--- machines-nok8s/apps/rebuildah/rebuildah.nix
+++ machines-nok8s/apps/rebuildah/rebuildah.nix
@@ -5,14 +5,12 @@
 { config, pkgs, ... }:
 let
   nix-experiment = import ./rebuildah-src.nix { inherit pkgs; };
-  nixConfigPath = "replacepathvariable";
-  useFlakes = "false";
 in
 {
   environment.systemPackages = with pkgs; [
     nix-experiment
   ];
   services.cron.systemCronJobs = [
-    "*/3 * * * * root nix-experiment -repopath /tmp/kek -nixconfig ${nixConfigPath} -repourl https://github.com/phonkd/inventory.git -ntfyurl https://notify.arnsi.ch/seltest -ntfytoken"
+    "*/3 * * * * root nix-experiment -repopath /tmp/kek -useflakes ${config.useFlakes} -nixconfig ${config.nixConfigPath} -repourl https://github.com/phonkd/inventory.git -ntfyurl https://notify.arnsi.ch/seltest -ntfytoken"
   ];
 }
