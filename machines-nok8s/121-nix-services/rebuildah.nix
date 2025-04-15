74c2938aba6e8880f89f732b2fddac7bea411e39
Phonkd <enst18.12@gmail.com>
2025-04-15 20:11:36 +0200

    temp

--- machines-nok8s/121-nix-services/rebuildah.nix
+++ machines-nok8s/121-nix-services/rebuildah.nix
@@ -1,7 +1,7 @@
 {
-  ntfytoken = builtins.readFile config.sops.secrets."ntfytoken".path;
+  ntfytoken = ${builtins.readFile config.sops.secrets."ntfytoken".path}
   services.cron.systemCronJobs = [
-    "*/3 * * * * root nix-experiment -repopath /tmp/kek -useflakes true -nixconfig machines-nok8s/121-nix-services/flake.nix -repourl https://github.com/phonkd/inventory.git -ntfyurl https://notify.arnsi.ch/seltest -ntfytoken"
+    "*/3 * * * * root nix-experiment -repopath /tmp/kek -useflakes true -nixconfig machines-nok8s/121-nix-services/flake.nix -repourl https://github.com/phonkd/inventory.git -ntfyurl https://notify.arnsi.ch/seltest -ntfytoken '${builtins.readFile config.sops.secrets."ntfytoken".path}'"
   ];
   # more is necessarcy (its in base)
 
