104239b6ccc2c2c066c7812c031573087a239898
Elis <elis.steiner@bedag.ch>
2025-04-16 18:33:59 +0200

    add for now unused teleport client config

---
+++ machines-nok8s/apps/teleport/teleport.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, ... }:
+let
+  vmhostname = config.networking.hostName;
+in
+{
+  sops.secrets.teleport_authkey = {
+    owner = "root";
+    key = "teleport_authkey";
+  };
+  services.teleport.enable = true;
+  services.teleport.settings = {
+    version = "v3";
+    teleport = {
+      nodename = "${vmhostname}";
+      # advertise_ip = "192.168.90.187";
+      auth_token = "${builtins.readFile config.sops.secrets."teleport_authkey".path}";
+      #auth_servers = [ "freakedyproxy.teleport.phonkd.net" ];
+      proxy_server = "teleport.phonkd.net:443";
+    };
+    ssh_service = {
+      enabled = true;
+      labels = {
+        #role = "client";
+        type = "node";
+      };
+    };
+    proxy_service.enabled = false;
+    auth_service.enabled = false;
+    ## sops key cant be used with remote build atm
+
+  };
+}
