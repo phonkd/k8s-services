e7187656f8df15c3bb4e240991ca5c9661887199
Phonkd <enst18.12@gmail.com>
2025-04-17 20:04:02 +0200

    ending homelab

--- machines-nok8s/121-nix-services/rebuildah.nix
+++ machines-nok8s/121-nix-services/rebuildah.nix
@@ -3,8 +3,7 @@
   imports = [
     ../apps/rebuildah/rebuildah.nix
   ];
-
-  services.cron.systemCronJobs = lib.mkAfter [ config.sops.secrets.ntfytoken ] [
+  services.cron.systemCronJobs = [
     "*/3 * * * * root nix-experiment -repopath /tmp/kek -useflakes true -nixconfig machines-nok8s/121-nix-services/flake.nix -repourl https://github.com/phonkd/inventory.git -ntfyurl https://notify.arnsi.ch/seltest -ntfytoken '${builtins.readFile config.sops.secrets."ntfytoken".path}'"
   ];
   # smore is necessarcy (its in base)
