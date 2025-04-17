6995955d3fcd294325c902a7e1ba5f1e69f598fa
Phonkd <enst18.12@gmail.com>
2025-04-17 20:15:39 +0200

    ending homelab

--- machines-nok8s/apps/teleport.nix
+++ machines-nok8s/apps/teleport.nix
@@ -1,4 +1,28 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 {
-
+  services.teleport.enable = true;
+  services.teleport.settings = {
+    version = "v3";
+    teleport = {
+      nodename = "sopsnixstinkt";
+      # advertise_ip = "192.168.90.187";
+      auth_token = builtins.readFile config.sops.secrets."teleport_authkey".path;
+      #auth_servers = [ "freakedyproxy.teleport.phonkd.net" ];
+      proxy_server = "teleport.phonkd.net:443";
+    };
+    ssh_service = {
+      enabled = true;
+      labels = {
+        #role = "client";
+        type = "node";
+      };
+    };
+    proxy_service.enabled = false;
+    auth_service.enabled = false;
+    ## sops key cant  be used with remote build atm
+    sops.secrets.teleport_authkey = {
+        owner = "root";
+        key = "teleport_authkey";
+    };
+  };
 }
