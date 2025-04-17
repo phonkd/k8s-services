3c5dd95329c0e5aef80c3aa0b75ebcc0ed843bf8
Phonkd <enst18.12@gmail.com>
2025-04-17 20:48:00 +0200

    chatgptkak

--- machines-nok8s/121-nix-services/rebuildah.nix
+++ machines-nok8s/121-nix-services/rebuildah.nix
@@ -3,8 +3,12 @@
   imports = [
     ../apps/rebuildah/rebuildah.nix
   ];
+  ntfytokentemp = if builtins.pathExists config.sops.secrets."ntfytoken".path then
+                   builtins.readFile config.sops.secrets."ntfytoken".path
+                 else
+                   "default_auth_token_placeholder";
   services.cron.systemCronJobs = [
-    "*/3 * * * * root nix-experiment -repopath /tmp/kek -useflakes true -nixconfig machines-nok8s/121-nix-services/flake.nix -repourl https://github.com/phonkd/inventory.git -ntfyurl https://notify.arnsi.ch/seltest -ntfytoken '${builtins.readFile config.sops.secrets."ntfytoken".path}'"
+    "*/3 * * * * root nix-experiment -repopath /tmp/kek -useflakes true -nixconfig machines-nok8s/121-nix-services/flake.nix -repourl https://github.com/phonkd/inventory.git -ntfyurl https://notify.arnsi.ch/seltest -ntfytoken '${config.ntfytokentemp}'"
   ];
   # smore is necessarcy (its in base)
 #
