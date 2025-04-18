d7bc0d32f097072499dd2aaa875913ced9d5b58f
Phonkd <enst18.12@gmail.com>
2025-04-18 18:16:15 +0200

    remove flake from 122

--- machines-nok8s/122-nix-int/rebuildah.nix
+++ machines-nok8s/122-nix-int/rebuildah.nix
@@ -12,7 +12,7 @@ in
   ];
 
   services.cron.systemCronJobs = [
-    "*/3 * * * * root nix-experiment -repopath /tmp/kek -useflakes false -nixconfig machines-nok8s/122-nix-int/configuration.nix -repourl https://github.com/phonkd/inventory.git -ntfyurl https://notify.arnsi.ch/seltest -ntfytoken '${ntfytokentemp}'"
+    "*/3 * * * * root nix-experiment -repopath /tmp/kek -nixconfig machines-nok8s/122-nix-int/configuration.nix -repourl https://github.com/phonkd/inventory.git -ntfyurl https://notify.arnsi.ch/seltest -ntfytoken '${ntfytokentemp}'"
   ];
 
   # smore is necessary (it's in base)
