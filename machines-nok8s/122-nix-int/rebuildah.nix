795a3a4b390ac96b4ed12a8b67e07e19ca4fa727
Phonkd <enst18.12@gmail.com>
2025-04-18 19:27:32 +0200

    just to see

--- machines-nok8s/122-nix-int/rebuildah.nix
+++ machines-nok8s/122-nix-int/rebuildah.nix
@@ -12,7 +12,7 @@ in
   ];
 
   services.cron.systemCronJobs = [
-    "*/3 * * * * root nix-experiment -repopath /tmp/kek -nixconfig machines-nok8s/122-nix-int/configuration.nix -repourl https://github.com/phonkd/inventory.git -ntfyurl https://notify.arnsi.ch/seltest -ntfytoken '${ntfytokentemp}'"
+    "*/3 * * * * root nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos:nixos-config=/etc/nixos/configuration.nix:/nix/var/nix/profiles/per-user/root/channels nix-experiment -repopath /tmp/kek -nixconfig machines-nok8s/122-nix-int/configuration.nix -repourl https://github.com/phonkd/inventory.git -ntfyurl https://notify.arnsi.ch/seltest -ntfytoken '${ntfytokentemp}'"
   ];
 
   # smore is necessary (it's in base)asdf
