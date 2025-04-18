6987caa83fc3df8d3c55dccdc7dd6e30ee166679
Phonkd <enst18.12@gmail.com>
2025-04-18 15:46:46 +0200

    dev vm rebuildah

--- machines-nok8s/10112-dev-vm/rebuildah.nix
+++ machines-nok8s/10112-dev-vm/rebuildah.nix
@@ -1,11 +1,13 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
+
+let
+  ntfytokentemp = if builtins.pathExists config.sops.secrets."ntfytoken".path then
+                    builtins.readFile config.sops.secrets."ntfytoken".path
+                  else
+                    "default_auth_token_placeholder";
+in
 {
-  imports = [
-    ../apps/rebuildah/rebuildah.nix
-  ];
   services.cron.systemCronJobs = [
-    "*/3 * * * * root nix-experiment -repopath /tmp/kek -useflakes true -nixconfig machines-nok8s/121-nix-services/flake.nix -repourl https://github.com/phonkd/inventory.git -ntfyurl https://notify.arnsi.ch/seltest -ntfytoken '${builtins.readFile config.sops.secrets."ntfytoken".path}'"
+    "*/3 * * * * root nix-experiment -repopath /tmp/kek -useflakes false -nixconfig machines-nok8s/10112-dev-vm/configuration.nix -repourl https://github.com/phonkd/inventory.git -ntfyurl https://notify.arnsi.ch/seltest -ntfytoken '${ntfytokentemp}'"
   ];
-  # smore is necessarcy (its in base)
-#
 }
