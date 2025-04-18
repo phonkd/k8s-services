5fdfc84fb8f4a0caa58fa36a4a2c9a2633221b33
Phonkd <enst18.12@gmail.com>
2025-04-18 21:35:28 +0200

    imich

--- machines-nok8s/apps/reverseproxy.nix
+++ machines-nok8s/apps/reverseproxy.nix
@@ -27,5 +27,14 @@ in
         }
       }
     '';
+    virtualHosts."pve.nix-services.phonkd.net".extraConfig = ''
+      reverse_proxy {
+        to 192.168.1.46:8006
+        transport http {
+            tls
+            tls_insecure_skip_verify
+        }
+      }
+    '';
   };
 }
