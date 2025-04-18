508c8a33a7d48a8181ee8308f0049f59da8afb5a
Phonkd <enst18.12@gmail.com>
2025-04-18 21:45:40 +0200

    immich

--- machines-nok8s/apps/immich.nix
+++ machines-nok8s/apps/immich.nix
@@ -4,7 +4,7 @@
   services.immich.port = 2283;
   services.caddy = {
     virtualHosts."immich.nix-services.phonkd.net".extraConfig = ''
-      reverse_proxy 127.0.0.1:2283
+      reverse_proxy localhost:2283
     '';
   };
 }
