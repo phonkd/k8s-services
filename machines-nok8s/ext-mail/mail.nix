31024553cfa8ae8ee2ed8aad71037e5f19580a60
Phonkd <enst18.12@gmail.com>
2025-04-19 17:42:09 +0200

    change commit

--- machines-nok8s/ext-mail/mail.nix
+++ machines-nok8s/ext-mail/mail.nix
@@ -12,10 +12,10 @@ in
   imports = [
     (builtins.fetchTarball {
       # Pick a release version you are interested in and set its hash, e.g.
-      url = "https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/archive/v2.3.0/nixos-mailserver-v2.3.0.tar.gz";
+      url = "https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/archive/nixos-24.11/nixos-mailserver-nixos-24.11.tar.gz";
       # To get the sha256 of the nixos-mailserver tarball, we can use the nix-prefetch-url command:
       # release="nixos-23.05"; nix-prefetch-url "https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/archive/${release}/nixos-mailserver-${release}.tar.gz" --unpack
-      sha256 = "0lpz08qviccvpfws2nm83n7m2r8add2wvfg9bljx9yxx8107r919";
+      sha256 = "63209b1def2c9fc891ad271f474a3464a5833294;
     })
   ];
   mailserver = {
