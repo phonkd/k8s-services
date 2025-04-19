964fb7759643836dea297376775f6a40520834a5
Elis <elis.steiner@bedag.ch>
2025-04-19 19:03:12 +0200

    for when on win

--- machines-nok8s/10112-dev-vm/configuration.nix
+++ machines-nok8s/10112-dev-vm/configuration.nix
@@ -16,4 +16,10 @@
       ../apps/teleport.nix
     ];
   sops.secrets.teleport_authkey = {};
+  users.users."phonkd".openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICC9CNRY2Taox2Trbsj7nBf52V73gGmJLM20+b2w1n5z phonkd@DESKTOP-F5QQLIG"
+    # content of authorized_keys file
+    # note: ssh-copy-id will add user@your-machine after the public key
+    # but we can remove the "@your-machine" part
+  ];
 }
